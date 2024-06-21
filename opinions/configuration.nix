8e0a50c0f17cca8f6312e4937c58ddd1baaacf02
Chris Guida <chrisguida@gmail.com>
2024-06-21 14:16:07 -0600

    add lightning and new hostname

--- opinions/configuration.nix
+++ opinions/configuration.nix
@@ -5,11 +5,11 @@
     ./quirks.nix
     ./recommended.nix
   ];
+  networking.hostName = "nix-enchilada";
   nixpkgs.config.allowUnfree = true;
   services.openssh.enable = true;
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      # FIXME: Replace this with your SSH pubkey
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAyg23mUQq55zHvcjo+F8bVXDQ33b4uIhiYU99V3lX1 cguida@cg-acer"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQjOgrgPaeaCAMMgNpjayLgj6EPC4m32MSCklYyYsuU cguida@cg-lenovo"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHe7dngSz9Krt4V0dH3EZkwOiim/vEYCgDNb3ENxplJ6 cguida@nix-beast"
@@ -19,6 +19,8 @@
     cguida = {
       isNormalUser = true;
       extraGroups = [
+        "bitcoin"
+        "clightning"
         "dialout"
         "feedbackd"
         "networkmanager"
@@ -27,14 +29,24 @@
       ];
     };
   };
+
+  # nix-bitcoin stuff
+  nix-bitcoin.generateSecrets = true;
   services.bitcoind = {
     enable = true;
     disablewallet = true;
     txindex = true;
-#    rpc = {
-#      address = "0.0.0.0";
-#      allowip = [ "192.168.100.0/24" "100.75.154.0/24" ];
-#    };
   };
-  nix-bitcoin.generateSecrets = true;
+  services.clightning = {
+    enable = true;
+    address = "0.0.0.0";
+#    plugins.summary.enable = true;
+
+    # enable some cool CLN features
+#    extraConfig = ''
+#      experimental-offers
+#      experimental-dual-fund
+#      experimental-splicing
+#    '';
+  };
 }
