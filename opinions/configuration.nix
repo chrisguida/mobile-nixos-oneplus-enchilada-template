159708820a2fc1c89c484fc93a594672e969d99f
Chris Guida <chrisguida@gmail.com>
2024-06-21 20:51:44 +0000

    add tailscale and cguida ssh keys

--- opinions/configuration.nix
+++ opinions/configuration.nix
@@ -7,7 +7,12 @@
   ];
   networking.hostName = "nix-enchilada";
   nixpkgs.config.allowUnfree = true;
+
+  # networking services
   services.openssh.enable = true;
+  services.tailscale.enable = true;
+
+  # users
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAyg23mUQq55zHvcjo+F8bVXDQ33b4uIhiYU99V3lX1 cguida@cg-acer"
@@ -17,10 +22,32 @@
   };
   users.users = {
     cguida = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAyg23mUQq55zHvcjo+F8bVXDQ33b4uIhiYU99V3lX1 cguida@cg-acer"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQjOgrgPaeaCAMMgNpjayLgj6EPC4m32MSCklYyYsuU cguida@cg-lenovo"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHe7dngSz9Krt4V0dH3EZkwOiim/vEYCgDNb3ENxplJ6 cguida@nix-beast"
+      ];
       isNormalUser = true;
       extraGroups = [
         "bitcoin"
         "clightning"
+        "dialout"
+        "feedbackd"
+        "networkmanager"
+        "video"
+        "wheel"
+      ];
+    };
+  };
+  users.users = {
+    defaultUser = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAyg23mUQq55zHvcjo+F8bVXDQ33b4uIhiYU99V3lX1 cguida@cg-acer"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQjOgrgPaeaCAMMgNpjayLgj6EPC4m32MSCklYyYsuU cguida@cg-lenovo"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHe7dngSz9Krt4V0dH3EZkwOiim/vEYCgDNb3ENxplJ6 cguida@nix-beast"
+      ];
+      isNormalUser = true;
+      extraGroups = [
         "dialout"
         "feedbackd"
         "networkmanager"
@@ -40,13 +67,5 @@
   services.clightning = {
     enable = true;
     address = "0.0.0.0";
-#    plugins.summary.enable = true;
-
-    # enable some cool CLN features
-#    extraConfig = ''
-#      experimental-offers
-#      experimental-dual-fund
-#      experimental-splicing
-#    '';
   };
 }
